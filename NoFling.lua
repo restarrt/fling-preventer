85cef40035072d15d6ef6243b46e534a6cff8c22
reestart <89030769+restarrt@users.noreply.github.com>
2021-12-08 20:06:00 +0200

    Update NoFling.lua

--- NoFling.lua
+++ NoFling.lua
@@ -4,12 +4,16 @@
 local runservice = game["Run Service"]
 local function preventfling()
    if game.Players.LocalPlayer.Character then
-       for i,v in pairs(workspace:GetDescendants()) do
-           if not v:IsDescendantOf(game.Players.LocalPlayer.Character) and v.ClassName == "Part" then
-              v.Velocity = Vector3.new(0,0,0) --get rid of velocity (prime issue)
-              v.RotVelocity = Vector3.new(0,0,0) --rotation velocity, what those skidded flingerz use.
+       for i,v in pairs(game.Players:GetPlayers()) do
+           if not v == game.Players.LocalPlayer and v.Character then
+              for i,v in pairs(v.Character:GetDescendants()) do
+                 if v.ClassName == "Part" then
+                    v.Velocity = Vector3.new(0,0,0) --get rid of velocity (prime issue)
+                    v.RotVelocity = Vector3.new(0,0,0) --rotation velocity, what those skidded flingerz use.
+                 end
+              end
            end
        end
     end
 end
-runservice.Stepped:connect(preventfling)
+runservice.Heartbeat:connect(preventfling)
